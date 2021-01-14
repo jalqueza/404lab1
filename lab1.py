aa1fd560e8d30449b65b3cca03fc5c65138a9d14
Jerwyn Alqueza <alqueza@ualberta.ca>
2021-01-13 18:00:29 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,6 +2,7 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get('https://www.google.ca')
+# r = requests.get('https://www.google.ca')
+r = requests.get('https://github.com/jalqueza/404lab1/blob/master/lab1.py')
 
 print(r.text)
